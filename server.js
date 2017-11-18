13b779b50c28e15c5eaa18e4c375af7fcc4674eb
Guilherme de Lima Conceição <guilherme@ztrade.com.br>
2017-11-18 11:45:41 -0200

    api inicial criada, referencia do config

--- server.js
+++ server.js
@@ -3,3 +3,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const botly = require('botly');
+const config = require('./config');
+
+
+let app = express();
+
+app.get('/', (req, res) => {
+    res.send('oi');
+});
+
+app.listen(config.port, () => {
+    console.log(`api ativa na porta: ${config.port}`);
+});
